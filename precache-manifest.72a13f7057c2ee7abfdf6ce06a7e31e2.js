5ee6c22470a4e7fc801006e5ebe3934c7616a4a2
ysumit99 <ysumit99@gmail.com>
2019-10-29 11:50:38 +0530

    Updates

---
+++ precache-manifest.72a13f7057c2ee7abfdf6ce06a7e31e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a7c5698762c88c2b45da4b43954c20",
+    "url": "/boggle/index.html"
+  },
+  {
+    "revision": "3f39644924285a582f6c",
+    "url": "/boggle/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "170b7f4b1c420d9af088",
+    "url": "/boggle/static/js/2.3aaf2e94.chunk.js"
+  },
+  {
+    "revision": "3f39644924285a582f6c",
+    "url": "/boggle/static/js/main.6e523275.chunk.js"
+  },
+  {
+    "revision": "a778fb9e2e60db9bd2d0",
+    "url": "/boggle/static/js/runtime-main.b0c90d1e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/boggle/static/media/logo.25bf045c.svg"
+  }
+]);
